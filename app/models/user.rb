e51712fd740016235b1bb2c81cabacfd9bf2b22a
小島英夫 <>
2020-10-01 21:49:41 +0900

    各種テーブルとmodelの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :articles, depndent: :destroy
+  has_many :coments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
