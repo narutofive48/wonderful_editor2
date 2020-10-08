2962940acad0c023d8efd69f9d971295e03d884a
小島英夫 <>
2020-10-08 23:43:58 +0900

    rubocop チェンジ

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "必要な情報が揃っている場合" do
+    let(:user) { build(:user) }
+    it "article登録できる" do
+      expect(article).to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "必要な情報が揃っている場合" do
+    let(:user) { build(:user) }
+    it "ユーザー登録できる" do
+      expect(user).to be_valid
+    end
+  end
+
+  context "名前のみ入力している場合" do
+    let(:user) { build(:user, email: nil, password: nil) }
+    it "エラーが発生する" do
+      expect(user).not_to be_valid
+    end
+  end
+
+  context "email がない場合" do
+    let(:user) { build(:user, email: nil) }
+    it "エラーが発生する" do
+      expect(user).not_to be_valid
+    end
+  end
+
+  context "password がない場合" do
+    let(:user) { build(:user, password: nil) }
+    it "エラーが発生する" do
+      expect(user).not_to be_valid
+    end
+  end
 end
