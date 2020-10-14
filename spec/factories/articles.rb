a6677b392dc9c62d6712ce360e3398c07ee1c0b4
小島英夫 <>
2020-10-14 23:02:51 +0900

    article RSpecの完成

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :article do
-
+    title {Faker::Lorem.characters(number: 10..50)}
+    body {"Mytext"}
+    user
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,14 +2,22 @@ require "rails_helper"
 RSpec.describe Article, type: :model do
   context "必要な情報が揃っているとき" do
     it "articleがつくられる" do
-      article = Article.new(id: 1,title: "test_title", body: "test_body", user_id: 1)
+      article = FactoryBot.build(:article)
+      # article = Article.new(id: 1,title: "test_title", body: "test_body", user_id: 1)
       expect(article).to be_valid
     end
   end
-  context "user_idが入っていないとき" do
-    it "articleが作られない" do
-      article = Article.new(id: 1,title: "test_title", body: "test_body", user_id: nil)
+  context "titleが入ってないとき" do
+    let(:article){FactoryBot.build(:article, title: nil)}
+    fit "articleはつくられない" do
+      # article = FactoryBot.build(:article, title: nil)
       expect(article).to be_invalid
+    end
+  end
+  context "bodyeが入ってないとき" do
+    it "articleはつくられる" do
+      article = FactoryBot.build(:article, body: nil)
+      expect(article).to be_valid
     end
   end
 end
