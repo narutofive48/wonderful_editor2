4aa184eb2a222f60a109109b576bdf2be543f179
小島英夫 <>
2020-10-26 23:29:15 +0900

    task7-3 serializer 実装、テストの実装の途中

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title {Faker::Lorem.characters(number: 10..50)}
-    body {"Mytext"}
+    title {Faker::Lorem.word}
+    body {Faker::Lorem.sentence}
     user
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 RSpec.describe Article, type: :model do
   context "必要な情報が揃っているとき" do
+    let(:article){build(:article)}
     it "articleがつくられる" do
-      article = FactoryBot.build(:article)
       # article = Article.new(id: 1,title: "test_title", body: "test_body", user_id: 1)
       expect(article).to be_valid
     end
   end
   context "titleが入ってないとき" do
     let(:article){FactoryBot.build(:article, title: nil)}
-    fit "articleはつくられない" do
+    it "articleはつくられない" do
       # article = FactoryBot.build(:article, title: nil)
       expect(article).to be_invalid
     end
