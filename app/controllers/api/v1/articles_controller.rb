ecf49461f345ebd203432fb008e56b12e615fecb
小島英夫 <>
2020-11-08 20:09:54 +0900

    wonderful_editor task7-4と7-5を実装しました

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,5 +4,17 @@ module Api::V1
       articles = Article.order(updated_at: :desc)
       render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
     end
+    def show
+      article = Article.find(params[:id])
+      render json: article
+    end
+    def create
+      article = current_user.articles.create!(article_params)
+      render json: article
+    end
+    private
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
   end
 end
