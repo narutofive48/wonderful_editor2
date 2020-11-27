41a4c0f771973581d935432b4992485ec10a220c
小島英夫 <>
2020-11-27 21:34:53 +0900

    task9-2完全装備しました

--- spec/requests/api/v1/auth/sessions_spec.rb
+++ spec/requests/api/v1/auth/sessions_spec.rb
@@ -1,42 +1,47 @@
 require "rails_helper"
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
-  describe "POST /v1/auth/sign_in" do
+  describe "POST /api/v1/auth/sign_in" do
     subject { post(api_v1_user_session_path, params: params) }
-    context "必要な情報が存在するとき" do
-      let(:params){ attributes_for(:user) }
-      it "ユーザーの新規登録ができる" do
-        expect { subject }.to change { User.count }.by(1)
-        expect(response).to have_http_status(:ok)
-        res = JSON.parse(response.body)
-        expect(res["data"]["email"]).to eq(User.last.email)
-      end
-      it "header 情報を取得することができる" do
+
+    context "登録済のユーザー情報を送信したとき" do
+      let(:user){ create(:user) }
+      let(:params){ attributes_for(:user, email: user.email, password: user.password) }
+
+      it "ログインできる" do
         subject
         header = response.header
-        binding.pry
         expect(header["access-token"]).to be_present
         expect(header["client"]).to be_present
-        expect(header["expiry"]).to be_present
         expect(header["uid"]).to be_present
-        expect(header["token-type"]).to be_present
+        expect(response).to have_http_status(:ok)
       end
     end
-    context "nameが存在しないとき" do
-      let(:params){ attributes_for(:user, name: nil) }
-      it "エラーする" do
-        expect{ subject }.to change { User.count }.by(0)
+    context "emailが一致しないとき" do
+      let(:user){ create(:user) }
+      let(:params){ attributes_for(:user, email: "hoge", password: user.password) }
+      it "ログインできない" do
+        subject
         res = JSON.parse(response.body)
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(res["errors"]["name"]).to include "can't be blank"
+        header = response.header
+        expect(res["errors"]).to include "Invalid login credentials. Please try again."
+        expect(header["access-token"]).to be_blank
+        expect(header["client"]).to be_blank
+        expect(header["uid"]).to be_blank
+        expect(response).to have_http_status(:unauthorized)
       end
     end
-    context "emailが存在しないとき" do
-      let(:params){ attributes_for(:user, email: nil) }
-      it "エラーする" do
-        expect{ subject }.to change { User.count }.by(0)
+    context "passwordが一致しないとき" do
+      let(:user){ create(:user)}
+      let(:params){ attributes_for(:user, email: user.email, password: "hoge")}
+      it "ログインできない" do
+        subject
         res = JSON.parse(response.body)
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(res["errors"]["email"]).to include "can't be blank"
+        header = response.header
+        expect(res["errors"]).to include "Invalid login credentials. Please try again."
+        expect(header["access-token"]).to be_blank
+        expect(header["client"]).to be_blank
+        expect(header["uid"]).to be_blank
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
